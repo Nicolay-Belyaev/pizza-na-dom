a303cea08ee4fa7fb83b5c012c927339fdb8f099
MavrinSergey <mavrin_sergei@list.ru>
2024-06-21 22:36:30 +0400

    добавление данных для отображение карточек и верстка главной страницы

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,32 +1,90 @@
 <template>
   <div>
-    Это главная страница
-    <UiButton :theme="theme" :click-handler="changeTheme">
-      close
-      <UiIconsCross color="#d3d3d3"/>
-    </UiButton>
-    <UiSuggestionInput :func="addressSuggestions"/>
-    <UiSuggestionInput :func="emailSuggestions"/>
+    <CardsRenderer :pizza-cards="pizzas"></CardsRenderer>
   </div>
 </template>
 
 <script setup lang="ts">
-import {addressSuggestions} from "~/app/api/addressSuggestions";
-import {emailSuggestions} from "~/app/api/emailSuggestions";
-
-const theme = ref('light')
-const buttons = ref([
-  {value: 1, title: '1'},
-  {value: 5, title: '5'},
-  {value: 30, title: '30'}
-])
-const changeTheme = function () {
-  if (theme.value === 'black') {
-    theme.value = 'light'
-  } else {
-    theme.value = 'black'
+const pizzas = ref([
+  {
+    id: "1",
+    name: "Манго",
+    description: "Томатный соус, моцарелла, творожный сыр, пюре манго, руккола.",
+    image: "https://static.tildacdn.com/stor3137-3066-4533-b638-666639636363/11848312.jpg",
+    sizes: [
+      { label: "Small", value: 25, checked: true },
+      { label: "Medium", value: 30, checked: false },
+      { label: "Large", value: 35, checked: false }
+    ],
+    prices: [485, 715, 895],
+    toppings: ["Basil"]
+  },
+  {
+    id: "2",
+    name: "Терияки пицца",
+    description: "Classic American pizza with tomato sauce, mozzarella cheese, and pepperoni",
+    image: "https://static.tildacdn.com/stor3137-3066-4533-b638-666639636363/11848312.jpg",
+    sizes: [
+      { label: "Small", value: 25, checked: true },
+      { label: "Medium", value: 30, checked: false },
+      { label: "Large", value: 35, checked: false }
+    ],
+    prices: [485, 715, 895],
+    toppings: ["Pepperoni"]
+  },
+  {
+    id: "3",
+    name: "Сальса",
+    description: "Pizza loaded with fresh veggies like bell peppers, mushrooms, onions, and olives",
+    image: "https://optim.tildacdn.com/stor3365-3731-4962-b936-383262666637/-/format/webp/74219337.jpg",
+    sizes: [
+      { label: "Small", value: 25, checked: true },
+      { label: "Medium", value: 30, checked: false },
+      { label: "Large", value: 35, checked: false }
+    ],
+    prices: [485, 715, 895],
+    toppings: ["Bell Peppers", "Mushrooms", "Onions", "Olives"]
+  },
+  {
+    id: "4",
+    name: "XXXX-самая острая пицца в городе",
+    description: "Pizza loaded with fresh veggies like bell peppers, mushrooms, onions, and olives",
+    image: "https://optim.tildacdn.com/stor3137-3066-4533-b638-666639636363/-/resize/312x/-/format/webp/11848312.jpg",
+    sizes: [
+      { label: "Small", value: 25, checked: true },
+      { label: "Medium", value: 30, checked: false },
+      { label: "Large", value: 35, checked: false }
+    ],
+    prices: [485, 715, 895],
+    toppings: ["Bell Peppers", "Mushrooms", "Onions", "Olives"]
+  },
+  {
+    id: "5",
+    name: "Макс Пейн",
+    description: "Pizza loaded with fresh veggies like bell peppers, mushrooms, onions, and olives",
+    image: "https://optim.tildacdn.com/tild3166-6665-4265-b338-336133376130/-/format/webp/photo.jpg",
+    sizes: [
+      { label: "Small", value: 25, checked: true },
+      { label: "Medium", value: 30, checked: false },
+      { label: "Large", value: 35, checked: false }
+    ],
+    prices: [485, 715, 895],
+    toppings: ["Bell Peppers", "Mushrooms", "Onions", "Olives"]
+  },
+  {
+    id: "6",
+    name: "Пепперони",
+    description: "Pizza loaded with fresh veggies like bell peppers, mushrooms, onions, and olives",
+    image: "https://optim.tildacdn.com/tild3561-6630-4162-b362-613865343931/-/format/webp/_.jpg",
+    sizes: [
+      { label: "Small", value: 25, checked: true },
+      { label: "Medium", value: 30, checked: false },
+      { label: "Large", value: 35, checked: false }
+    ],
+    prices: [485, 715, 895],
+    toppings: ["Bell Peppers", "Mushrooms", "Onions", "Olives"]
   }
-}
+])
 </script>
 
 <style scoped>
