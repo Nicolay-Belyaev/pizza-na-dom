32196a79c3a048d955f148e4d4a58a64e895fb00
MavrinSergey <mavrin_sergei@list.ru>
2024-06-21 22:39:00 +0400

    стилизация

---
+++ src/shared/CardsRenderer.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul class="pizzas">
+    <li v-for="pizza in pizzaCards">
+      <UiCard :pizza="pizza"/>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  pizzaCards: {
+    type: Array<IPizza>,
+    default: {} // тут бы неплохо сделать заглушку
+  }
+})
+</script>
+
+
+<style scoped>
+.pizzas {
+  margin-bottom: 40px;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
+  grid-template-rows: auto;
+  column-gap: 25px;
+  row-gap: 30px;
+  justify-items: center;
+}
+</style>
---
+++ src/shared/ui/BaseButton.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+
+defineProps({
+  form: {
+    type: String,
+    default: 'rectangle'
+  },
+  color: {
+    type: String,
+    default: "light"
+  }
+})
+</script>
+
+<template>
+  <button class="btn"
+      :class="`btn__${form} btn__${color}`">
+    <slot/>
+  </button>
+</template>
+
+<style scoped lang="scss">
+.btn {
+  height: 35px;
+  border: none;
+  font-size: 13px;
+  cursor: pointer;
+}
+.btn__sizepicker_form {
+  padding-left: 15px;
+  padding-right: 15px;
+  border-radius: 20px;
+}
+.btn__sizepicker_color {
+  background-color: $brand;
+  color: $textColor2;
+}
+</style>
