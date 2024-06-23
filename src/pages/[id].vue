67f026da7e41fb8492efe614678f2b607da5c432
MavrinSergey <mavrin_sergei@list.ru>
2024-06-23 22:51:52 +0400

    Добавление на страницу пиццы чекбоксов

--- src/pages/[id].vue
+++ src/pages/[id].vue
@@ -1,13 +1,50 @@
 <script setup lang="ts">
+import {useRoute} from 'vue-router';
+import {usePizzaStore} from "~/app/stores/pizzaStore";
 
+const nuxtApp = useNuxtApp();
+const route = useRoute();
+const pizza = usePizzaStore().getPizzaById(`${route.params.id}`);
 </script>
 
 <template>
-  <div>
-    Это страница pizza id: {{ $route.params.id }}
+  <div class="pizza">
+    <img :src="`${pizza.image}`">
+    <div class="pizza__constructor">
+      <h2>{{ pizza.name }}</h2>
+      <p>{{ pizza.prices[0] }}р</p>
+      <div>
+        <div>выбирите размер</div>
+        <UiWidgetsRadioButtons :checkboxes="pizza.sizes"/>
+      </div>
+
+      <div>
+        <UiCheckboxWrapper :toppings="pizza.toppings"/>
+      </div>
+
+      <select>
+
+      </select>
+      <UiBaseButton form="sizepicker-form"
+                    color="sizepicker-color">добавить в корзину
+      </UiBaseButton>
+    </div>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.pizza {
+  padding-left: 30px;
+  display: flex;
+  gap: 30px;
 
+  &__constructor {
+    @include flexd();
+    gap: 20px;
+  }
+}
+
+img {
+  width: 440px;
+}
 </style>
